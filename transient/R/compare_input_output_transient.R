6cb2e98a8b90b889fecfae010be76e31b5afc59b
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-09-07 18:39:09 +0900

    Add: add 'compare_input_output_transient.R' to visualize input and output.

---
+++ transient/R/compare_input_output_transient.R
@@ -0,0 +1,16 @@
+data <- read.csv("../csv/result_to_compare_transient.csv", header=FALSE)
+
+data_index <- 20  # the numbr of data which we want to
+
+input <- data[data_index * 2 + 1,]
+output <- data[data_index * 2 + 2,]
+
+TIMESTEP <- length(data[1,])    # the length of X or Y 
+
+t <- 1:TIMESTEP
+
+# comparison
+plot(t, input, type = "l", ylim=c(0,1), xlab = "Time step", ylab = "value", col = 'red')
+par(new = TRUE)   #  Overwrite
+plot(t, output, type = 'l', ylim=c(0,1), xlab = '', ylab = '', col = 'blue') 
+
